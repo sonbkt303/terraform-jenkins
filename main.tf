dd2f9e767176f3b4dd088fb56587937054199a19
mike EHU8RD8 <mike.bui@gmail.com>
2024-04-13 18:35:52 +0700

    Update

--- main.tf
+++ main.tf
@@ -1,3 +1,42 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-series-s3-backend-01"
+    key            = "terraform-jenkins"
+    region         = "us-west-2"
+    encrypt        = true
+    # role_arn       = "arn:aws:iam::428208402376:role/Terraform-SeriesS3BackendRole"
+    dynamodb_table = "terraform-series-s3-backend"
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.server.public_ip
 }
